f388a636bc41136961f9bf6e3369e8736902c5ca
ewen_ <ewenhenderson@gmail.com>
2021-05-26 21:48:34 +0100

    build db

---
+++ build-db.sh
@@ -0,0 +1,2 @@
+cd data
+csvs-to-sqlite clubs.csv events.csv artists.csv promoters.csv ldn-club-nights.db
--- publish-datasette.sh
+++
@@ -1,6 +0,0 @@
-cd data
-csvs-to-sqlite clubs.csv events.csv artists.csv promoters.csv ldn-club-nights.db
-gcloud components install beta
-gcloud config set run/region europe-west2
-gcloud config set project club-cooking-datasette
-datasette publish cloudrun ldn-club-nights.db --service ldn-club-nights -m ../metadata.json
